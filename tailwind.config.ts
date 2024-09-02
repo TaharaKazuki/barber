3f674c5e5b386b15b8173b67da4eadb6df11f22d
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-09-02 15:10:53 +0900

    add assets and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,44 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import { DEFAULT_CIPHERS } from 'tls';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    screens: {
+      sm: '640px',
+      md: '778px',
+      lg: '960px',
+      xl: '1430px',
+    },
+    fontFamily: {
+      primary: 'var(--font-cormorant_upright)',
+      secondary: 'var(--font-open_sans)',
+    },
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#100e0e',
+        },
+        secondary: {
+          DEFAULT: '#787f8a',
+        },
+        accent: {
+          DEFAULT: '#c7a17a',
+          hover: '#a08161',
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        hero_overlay: "url('/assets/hero/hero-overlay.png')",
+        opening_hours: "url('/assets/opening-hours/bg.png')",
+        footer: "url('/assets/footer/bg.png')",
       },
     },
   },
